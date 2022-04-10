61d98c25f70c2613f7c44d4c9a76b83b32fd4ae5
korsar312 <69364209+korsar312@users.noreply.github.com>
2022-04-10 21:33:48 +0300

    Доработка

--- src/Component/3_Substances/AsidePanel/AsidePanelNavigation/AsidePanelNavigation.tsx
+++ src/Component/3_Substances/AsidePanel/AsidePanelNavigation/AsidePanelNavigation.tsx
@@ -5,61 +5,64 @@ import DropMenu from "../../../2_Molecules/DropMenu/DropMenu";
 import {navLink} from "./AsidePanelNavigation.list";
 
 interface IAsidePanelNavigation {
-  filter?: string
+	filter?: string
 }
+
 
 /**
  * Навигация для боковой панели
  * @param props.navigate - объект навигации
  */
 const AsidePanelNavigation: FC<IAsidePanelNavigation> = (props) => {
-  const {filter = ''} = props
+	const {filter = ''} = props
 
-  const navigate = filter ? navLink : navLink
+	const navigate = filter === '' ? navLink : applyFilter(navLink)
 
-  function recursFilter(obj: typeof navigate): typeof navigate[] {
+	function applyFilter(val: typeof navLink){
+		return val.reduce((acc: typeof navLink, cur)=>{
 
-    return obj.reduce((acc, cur)=>{
-      if(cur.children){//использовать SPLISE
+			if(cur.children){
+				const newArrLink = applyFilter(cur.children)
+				newArrLink.length !== 0 && acc.push({...cur, children: newArrLink})
+			} else {
+				cur.name.toLowerCase().includes(filter.toLowerCase()) && acc.push(cur)
+			}
 
-      }
-      // @ts-ignore
-      cur.name.toLowerCase().includes(filter) && acc.push(cur)
-      return acc
-    },[])
-  }
+			return acc
+		}, [])
+	}
 
 
-    function recursRenderButton(navObj: typeof navigate[number]): JSX.Element {
-    if (navObj.children) {
-      return <React.Fragment key={navObj.id}>
-        <DropMenu
-          isPaddingOn={true}
-          iconLeft={navObj.leftImg}
-          title={navObj.name}
-          extClass={styles.btn}
-        >
-          <>{navObj.children.map(el => recursRenderButton(el))}</>
-        </DropMenu></React.Fragment>
-    } else {
-      return <React.Fragment key={navObj.id}>
-        <ButtonStandard
-          extClass={`${styles.btn}`}
-          textStyle={"light_small"}
-          title={navObj.name}
-          click={navObj.click}
-          log={{element: AsidePanelNavigation.name}}
-          iconLeft={{icon: navObj.leftImg || undefined}}
-        />
-      </React.Fragment>
-    }
-  }
+	function recursRenderButton(navObj: typeof navigate[number]): JSX.Element {
+		if (navObj.children) {
+			return <React.Fragment key={navObj.id}>
+				<DropMenu
+					isPaddingOn={true}
+					iconLeft={navObj.leftImg}
+					title={navObj.name}
+					extClass={styles.btn}
+				>
+					<>{navObj.children.map(el => recursRenderButton(el))}</>
+				</DropMenu></React.Fragment>
+		} else {
+			return <React.Fragment key={navObj.id}>
+				<ButtonStandard
+					extClass={`${styles.btn}`}
+					textStyle={"light_small"}
+					title={navObj.name}
+					click={navObj.click}
+					log={{element: AsidePanelNavigation.name}}
+					iconLeft={{icon: navObj.leftImg || undefined}}
+				/>
+			</React.Fragment>
+		}
+	}
 
-  return (
-    <div className={styles.wrapper}>
-      {navigate.map(el => recursRenderButton(el))}
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			{navigate.map(el => recursRenderButton(el))}
+		</div>
+	);
 };
 
 export default AsidePanelNavigation;
